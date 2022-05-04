7670c2056882e9c6df83e9901716e1cee90df980
hooniec <cshoon1220@gmail.com>
2022-05-05 00:01:10 +1200

    Created models and views

---
+++ OnlineClothesStore/Controllers/ClothesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OnlineClothesStore.Models;
+
+namespace OnlineClothesStore.Controllers
+{
+    public class ClothesController : Controller
+    {
+        private StoreDatabaseEntities db = new StoreDatabaseEntities();
+
+        // GET: Clothes
+        public ActionResult Index()
+        {
+            return View(db.Clothes.ToList());
+        }
+
+        // GET: Clothes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cloth cloth = db.Clothes.Find(id);
+            if (cloth == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cloth);
+        }
+
+        // GET: Clothes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clothes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CId,Gender,Category,Condition,Color,Size,Brand,Location,Price")] Cloth cloth)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Clothes.Add(cloth);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cloth);
+        }
+
+        // GET: Clothes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cloth cloth = db.Clothes.Find(id);
+            if (cloth == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cloth);
+        }
+
+        // POST: Clothes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CId,Gender,Category,Condition,Color,Size,Brand,Location,Price")] Cloth cloth)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cloth).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cloth);
+        }
+
+        // GET: Clothes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cloth cloth = db.Clothes.Find(id);
+            if (cloth == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cloth);
+        }
+
+        // POST: Clothes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cloth cloth = db.Clothes.Find(id);
+            db.Clothes.Remove(cloth);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
